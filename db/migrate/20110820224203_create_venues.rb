d2ff010d78707f949f75481c8792c446d1871100
Leigh Caplan <lcaplan@onehub.com>
2011-08-20 15:53:00 -0700

    Venue migration

--- db/migrate/20110820224203_create_venues.rb
+++ db/migrate/20110820224203_create_venues.rb
@@ -1,6 +1,13 @@
 class CreateVenues < ActiveRecord::Migration
   def change
     create_table :venues do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+
+      t.text   :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110820201051) do
+ActiveRecord::Schema.define(:version => 20110820224203) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -30,5 +30,16 @@ ActiveRecord::Schema.define(:version => 20110820201051) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "venues", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
