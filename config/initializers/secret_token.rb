13fff1af20800c17f2058fce44a3716513eeb347
Leigh Caplan <lcaplan@onehub.com>
2011-08-20 12:06:36 -0700

    Yay, it's a Rails app!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchBot::Application.config.secret_token = 'a492b4288851a7ff49955c51681f74c10d7700e0e83f2a7eb55556b3ed56d7bb85f0d656e8748384b9ffa49331a9d7affa0949263e3116b53ab3f7573b915f29'
